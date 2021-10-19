71f0a8be7953dec1795206b624d16b905c103cb3
Mircea Matei <mircea.matei@ntgfreight.com>
2021-10-20 00:24:15 +0300

    Fixing the Deprecated property on LimitedRange to be in sync with WeatherForecast

--- DemoVersioning/Controllers/LimitedRangeVersionedBaseController.cs
+++ DemoVersioning/Controllers/LimitedRangeVersionedBaseController.cs
@@ -5,9 +5,10 @@ namespace ApiVersioningDemo.Controllers
     /// <summary>
     /// This class is a single point of changes for controllers that are available on limited range of versions
     /// Warning: Not recommended! Better approach is to use the ApiVersionNeutral
+    ///     - The Deprecated property is hard to be maintained
     /// </summary>
     [ApiController]
-    [ApiInheritableVersion("1.2")]
+    [ApiInheritableVersion("1.2", Deprecated = true)]
     [ApiInheritableVersion("2.0")]
     [Route("api/v{api-version:apiVersion}/[controller]")]
     public abstract class LimitedRangeVersionedBaseController : ControllerBase
